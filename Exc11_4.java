ad95e521bbda3b9d7ee5a5997a30d5700664dc97
Язов Михаил <100093644+MRxPosiTiVe@users.noreply.github.com>
2023-06-03 14:37:17 +0500

    Add files via upload

--- Exc11_4.java
+++ Exc11_4.java
@@ -2,24 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Exc11_4 extends JFrame {
 
-    private JTextField numberField;
+    private JTextField outputField;
     private JButton generateButton;
 
     public Exc11_4() {
         setTitle("Генератор случайных чисел");
-        setSize(200, 250);
+        setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
 
-        numberField = new JTextField(10);
-        numberField.setEditable(false);
+        outputField = new JTextField(10);
+        outputField.setEditable(false);
 
         generateButton = new JButton("Генерировать");
+        generateButton.setPreferredSize(new Dimension(100, 50)); // Установка размеров кнопки
         generateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -27,7 +29,7 @@ public class Exc11_4 extends JFrame {
             }
         });
 
-        panel.add(numberField, BorderLayout.CENTER);
+        panel.add(outputField, BorderLayout.CENTER);
         panel.add(generateButton, BorderLayout.SOUTH);
 
         getContentPane().add(panel);
@@ -35,12 +37,15 @@ public class Exc11_4 extends JFrame {
     }
 
     private void generateRandomNumbers() {
+        Random random = new Random();
         StringBuilder builder = new StringBuilder();
+
         for (int i = 0; i < 7; i++) {
-            int randomNumber = (int) (Math.random() * 100);
+            int randomNumber = random.nextInt(100);
             builder.append(randomNumber).append("\n");
         }
-        numberField.setText(builder.toString());
+
+        outputField.setText(builder.toString());
     }
 
     public static void main(String[] args) {
